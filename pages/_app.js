4778af84991870a55a6ae7df8207d723d7520f63
victorzottmann <zottmann.victor@gmail.com>
2021-07-14 00:02:51 +1000

    improve styling and add AlurakutMenu from AlurakutCommons.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import { AlurakutStyles } from '../src/lib/AlurakutCommons'
 
 const GlobalStyle = createGlobalStyle`
   * {
@@ -23,7 +24,10 @@ const GlobalStyle = createGlobalStyle`
     height: auto;
     display: block;
   }
+  
+  ${AlurakutStyles}
 `
+
 
 const theme = {
   colors: {
--- pages/index.js
+++ pages/index.js
@@ -2,25 +2,40 @@ import styled from 'styled-components'
 
 import MainGrid from '../src/components/MainGrid'
 import Box from '../src/components/Box'
+import { AlurakutMenu } from '../src/lib/AlurakutCommons'
+
+function ProfileSidebar(props) {
+  return (
+    <Box>
+      <img src={`https://github.com/${props.githubUser}.png`} style={{ borderRadius: '8px' }} />
+    </Box>
+  )
+}
 
 export default function Home() {
+  const usuarioAleatorio = 'victorzottmann'
+
   return (
-    <MainGrid>
-      <div className="profileArea" style={{ gridArea: 'profileArea' }}>
-        <Box>
-          <img src="https://github.com/victorzottmann.png" />
-        </Box>
-      </div>
-      <div className="welcomeArea" style={{ gridArea: 'welcomeArea' }}>
-        <Box>
-          Bem-vindo
-        </Box>
-      </div>
-      <div className="profileRelationsArea" style={{ gridArea:'profileRelationsArea' }}>
-        <Box>
-          Comunidades
-        </Box>
-      </div>
-    </MainGrid>
+    <>
+      <AlurakutMenu />
+      <MainGrid>
+        <div className="profileArea" style={{ gridArea: 'profileArea' }}>
+          <ProfileSidebar githubUser={usuarioAleatorio} />
+        </div>
+        <div className="welcomeArea" style={{ gridArea: 'welcomeArea' }}>
+          <Box>
+            Bem-vindo
+          </Box>
+        </div>
+        <div className="profileRelationsArea" style={{ gridArea:'profileRelationsArea' }}>
+          <Box>
+            Amigos
+          </Box>
+          <Box>
+            Comunidades
+          </Box>
+        </div>
+      </MainGrid>
+    </>
   ) 
 }
