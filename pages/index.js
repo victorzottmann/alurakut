a6d4eea53c30ae11e7e9522ff0bb52e41454f370
victorzottmann <zottmann.victor@gmail.com>
2021-07-14 00:36:56 +1000

    add and style ProfileRelations component

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,8 @@ import styled from 'styled-components'
 
 import MainGrid from '../src/components/MainGrid'
 import Box from '../src/components/Box'
-import { AlurakutMenu } from '../src/lib/AlurakutCommons'
+import { AlurakutMenu, OrkutNostalgicIconSet } from '../src/lib/AlurakutCommons'
+import { ProfileRelationsBoxWrapper } from '../src/components/ProfileRelations'
 
 function ProfileSidebar(props) {
   return (
@@ -14,6 +15,14 @@ function ProfileSidebar(props) {
 
 export default function Home() {
   const usuarioAleatorio = 'victorzottmann'
+  const pessoasFavoritas = [
+    'juunegreiros', 
+    'omariosouto', 
+    'peas', 
+    'rafaballerini', 
+    'marcobrunodev',
+    'felipefialho'
+  ]
 
   return (
     <>
@@ -24,13 +33,30 @@ export default function Home() {
         </div>
         <div className="welcomeArea" style={{ gridArea: 'welcomeArea' }}>
           <Box>
-            Bem-vindo
+            <h1 className="title">
+              Bem-vindo(a)
+            </h1>
+            <OrkutNostalgicIconSet />
           </Box>
         </div>
         <div className="profileRelationsArea" style={{ gridArea:'profileRelationsArea' }}>
-          <Box>
-            Amigos
-          </Box>
+          <ProfileRelationsBoxWrapper>
+            <h2 className="smallTitle">
+              Pessoas da Comunidade ({pessoasFavoritas.length})
+            </h2>
+            <ul>
+              {pessoasFavoritas.map((item) => {
+                return (
+                  <li>
+                    <a href={`/users/${item}`} key={item}>
+                      <img src={`https://github.com/${item}.png`} />
+                      <span>{item}</span>
+                    </a>
+                  </li>
+                )
+              })}
+            </ul>
+          </ProfileRelationsBoxWrapper>
           <Box>
             Comunidades
           </Box>
