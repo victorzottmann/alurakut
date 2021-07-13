f794444f5f0111ccd6052ce5f0d8fa7396772889
victorzottmann <zottmann.victor@gmail.com>
2021-07-13 19:24:50 +1000

    add basic styling to three main columns

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,21 @@
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
 
 const GlobalStyle = createGlobalStyle`
-  body {
+  * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+  }
+
+  body {
+    font-family: sans-serif;
+    background-color: #D9E6F6;
+  }
+
+  #__next {
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
   }
 `
 
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,45 @@
 import styled from 'styled-components'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
+// const Title = styled.h1`
+//   font-size: 50px;
+//   color: ${({ theme }) => theme.colors.primary};
+// `
+
+const Box = styled.div`
+  background: #fff;
+  border-radius: 8px;
+
+`
+
+const MainGrid = styled.main`
+  display: grid;
+  padding: 16px;
+  grid-gap: 10px;
+
+  @media(min-width: 860px) {
+    grid-template-areas: "profileArea welcomeArea profileRelationsArea";
+    grid-template-columns: 160px 618px 312px;
+  }
 `
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <MainGrid>
+      <div style={{ gridArea: 'profileArea' }}>
+        <Box>
+          Imagem
+        </Box>
+      </div>
+      <div style={{ gridArea: 'welcomeArea' }}>
+        <Box>
+          Bem-vindo
+        </Box>
+      </div>
+      <div style={{ gridArea:'profileRelationsArea' }}>
+        <Box>
+          Comunidades
+        </Box>
+      </div>
+    </MainGrid>
+  ) 
 }
