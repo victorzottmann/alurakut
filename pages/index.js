b70191c145566a15fc7b5449f8d1d20382c489ca
victorzottmann <zottmann.victor@gmail.com>
2021-07-17 19:37:15 +1000

    add prettier and nookies, begin auth setup on login form submit

--- pages/index.js
+++ pages/index.js
@@ -9,59 +9,59 @@ import ProfileRelations from '../src/components/ProfileRelations'
 function Home() {
   const [communities, setCommunities] = useState([])
   const [followers, setFollowers] = useState([])
-  
+
   const githubUser = 'victorzottmann'
 
   useEffect(() => {
     // GET request
     fetch('https://api.github.com/users/victorzottmann/followers')
-      .then(res => res.json())
-      .then(data => setFollowers(data))
+      .then((res) => res.json())
+      .then((data) => setFollowers(data))
 
     // GraphQL API
     fetch('https://graphql.datocms.com/', {
       method: 'POST',
       headers: {
-        'Authorization': 'cb94c76f7bea2e4fa1cb58ab83d405',
+        Authorization: 'cb94c76f7bea2e4fa1cb58ab83d405',
         'Content-Type': 'application/json',
-        'Accept': 'application/json',
+        Accept: 'application/json',
       },
-      body: JSON.stringify({'query': `query {
+      body: JSON.stringify({
+        query: `query {
           allCommunities {
             id
             title
             imageUrl
             creatorSlug
           }
-        }`
+        }`,
+      }),
+    })
+      .then((res) => res.json())
+      .then((jsonResponse) => {
+        const communitiesFromDatoCms = jsonResponse.data.allCommunities
+        console.log(communities)
+        setCommunities(communitiesFromDatoCms)
       })
-    })
-    .then(res => res.json())
-    .then(jsonResponse => {
-      const communitiesFromDatoCms = jsonResponse.data.allCommunities
-      console.log(communities)
-      setCommunities(communitiesFromDatoCms)
-    })
   }, [])
-  
+
   const handleCreateCommunity = (e) => {
-    e.preventDefault();
-    const formInputData = new FormData(e.target);
+    e.preventDefault()
+    const formInputData = new FormData(e.target)
 
     const community = {
       title: formInputData.get('title'),
       imageUrl: formInputData.get('image'),
       creatorSlug: githubUser,
     }
-    
+
     fetch('/api/communities', {
       method: 'POST',
       headers: {
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
       },
-      body: JSON.stringify(community)
-    })
-    .then(async (res) => {
+      body: JSON.stringify(community),
+    }).then(async (res) => {
       const dados = await res.json()
 
       console.log('dados:', dados.registroCriado)
@@ -86,39 +86,38 @@ function Home() {
           </Box>
 
           <Box>
-            <h2 className="subTitle">
-              What would you like to do?
-            </h2>
+            <h2 className="subTitle">What would you like to do?</h2>
             <form onSubmit={handleCreateCommunity}>
               <div>
-                <input 
-                  placeholder="What will be the name of your community?" 
-                  name="title" 
-                  aria-label="What will be the name of your community?" 
+                <input
+                  placeholder="What will be the name of your community?"
+                  name="title"
+                  aria-label="What will be the name of your community?"
                 />
               </div>
               <div>
-                <input 
-                  placeholder="Provide a URL to link to the image." 
-                  name="image" 
-                  aria-label="Provide a URL to link to the image." 
+                <input
+                  placeholder="Provide a URL to link to the image."
+                  name="image"
+                  aria-label="Provide a URL to link to the image."
                 />
               </div>
-              <button>
-                Create community
-              </button>
+              <button>Create community</button>
             </form>
           </Box>
         </div>
 
         {/* Third column */}
-        <div className="profileRelationsArea" style={{ gridArea:'profileRelationsArea' }}>
+        <div
+          className="profileRelationsArea"
+          style={{ gridArea: 'profileRelationsArea' }}
+        >
           <ProfileRelations title="Followers" items={followers} />
           <ProfileRelations title="Communities" items={communities} />
         </div>
       </MainGrid>
     </>
-  ) 
+  )
 }
 
 export default Home
--- src/components/ProfileSidebar/index.js
+++ src/components/ProfileSidebar/index.js
@@ -1,21 +1,24 @@
-import { AlurakutProfileSidebarMenuDefault } from '../../lib/AlurakutCommons';
+import { AlurakutProfileSidebarMenuDefault } from "../../lib/AlurakutCommons";
 
-import Box from '../Box';
+import Box from "../Box";
 
-function ProfileSidebar(props) {
+function ProfileSidebar({ githubUser }) {
   return (
     <Box as="aside">
-      <img src={`https://github.com/${props.githubUser}.png`} style={{ borderRadius: '8px' }} />
+      <img
+        src={`https://github.com/${githubUser}.png`}
+        style={{ borderRadius: "8px" }}
+      />
       <hr />
       <p>
-        <a className="boxLink" href={`https://github.com/${props.githubUser}`}>
-          @{props.githubUser}
+        <a className="boxLink" href={`https://github.com/${githubUser}`}>
+          @{githubUser}
         </a>
       </p>
       <hr />
       <AlurakutProfileSidebarMenuDefault />
     </Box>
-  )
+  );
 }
 
-export default ProfileSidebar
+export default ProfileSidebar;
